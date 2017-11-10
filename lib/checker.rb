1b788cd149d3021c54f9651e6e4730de292f0056
Paul Mannion <paul.mannion@storyful.com>
2017-11-10 17:25:35 +0000

    after talk

--- lib/checker.rb
+++ lib/checker.rb
@@ -2,17 +2,22 @@ class Checker
   #IN CASE I CANT COUNT TOO WELL
   TOLERANCE = 1
 
-  attr_reader :audience_count, :knowledge, :no_knowledge, :speaker_count
+  attr_reader :knowledge, :no_knowledge, :audience_count, :speaker_present
 
-  def initialize(audience_count, knowledge, no_knowledge, speaker_count)
-    @audience_count = audience_count
+  def initialize(knowledge, no_knowledge, audience_count, speaker_present)
     @knowledge = knowledge
     @no_knowledge = no_knowledge
-    @speaker_count = speaker_count
+    @audience_count = audience_count
+    @speaker_present = speaker_present
   end
 
   def valid_participation?
-    (knowledge + no_knowledge).between?(audience_count - TOLERANCE, audience_count + TOLERANCE)
+    if speaker_present
+      #Allowable range is between 19 and 21, allowing a tolerance of 1 each side (based on audience size of 20)
+      (knowledge + no_knowledge).between?(audience_count - TOLERANCE, audience_count + TOLERANCE)
+    else
+      false
+    end
   end
 
 end
--- spec/checker_spec.rb
+++ spec/checker_spec.rb
@@ -1,12 +1,14 @@
-require "checker"
+require 'checker'
 require 'byebug'
 
 describe Checker do
-  let(:checker) { Checker.new(audience_count, knowledge, no_knowledge, speaker_count) }
-  let(:audience_count) { 20 }
+  let(:checker) { Checker.new(knowledge, no_knowledge, audience_count, speaker_present) }
   let(:knowledge) { 10 }
   let(:no_knowledge) { 10 }
-  let(:speaker_count) { 1 }
+  let(:audience_count) { 20 }
+  let(:speaker_present) { true }
+
+  ####### STAGE 0 HAPPY PATH ########
 
   describe '#valid_participation?' do
     context 'when all audience members particpate' do
@@ -15,15 +17,15 @@ describe Checker do
       end
     end
 
-    # ####### STAGE 1 TOLERANCE ########
-    #
+    ####### STAGE 1 TOLERANCE ########
+
+
     # context 'when there is a count error of -1' do
     #   let(:no_knowledge) { 9 }
     #
     #   it 'Is inside the TOLERANCE & returns TRUE' do
     #     expect(checker.valid_participation?).to eq(true)
     #   end
-    #
     # end
     #
     # context 'when there is a count error of +1' do
@@ -32,95 +34,41 @@ describe Checker do
     #   it 'Is inside the TOLERANCE & returns TRUE' do
     #     expect(checker.valid_participation?).to eq(true)
     #   end
-    #
     # end
+
+
+
+    ############# STAGE 2 MISSING/INCORRECT AUDIENCE  ########
+
     #
-    # ####### STAGE 2 MISSING COUNTS ########
+    # context 'when audience is miscounted' do
+    #   let(:audience_count) { 22 }
     #
-    # context 'when I forget to count people with knowledge' do
-    #   let(:knowledge) { nil }
+    #   it 'returns false' do
+    #     expect(checker.valid_participation?).to eq(false)
+    #   end
+    # end
+
+
+
+    ############ STAGE 3 MISSING SPEAKER  ########
+
+
+    # context 'when speaker is NOT PRESENT' do
+    #   let(:speaker_present) { false }
     #
-    #   it 'Is outside the TOLERANCE & returns FALSE' do
+    #   it 'returns false' do
     #     expect(checker.valid_participation?).to eq(false)
     #   end
     # end
     #
-    # context 'when I forget to count people with no knowledge' do
-    #   let(:no_knowledge) { nil }
+    # context 'when speaker is nil' do
+    #   let(:speaker_present) { nil }
     #
-    #   it 'Is outside the TOLERANCE & returns FALSE' do
+    #   it 'returns false' do
     #     expect(checker.valid_participation?).to eq(false)
     #   end
     # end
-    #
-    #
-    # context 'when there is no audience count' do
-    #   let(:audience_count) { nil }
-    #
-    #   it 'returns Paul, Why are you here if there is no audience' do
-    #     expect(checker.valid_participation?).to eq('Paul, You forgot to count the audience total!')
-    #   end
-    # end
-    #
-    # context 'when there is no speaker' do
-    #   let(:speaker_count) { nil }
-    #
-    #   it 'returns Audience, the speaker could not make it!' do
-    #     expect(checker.valid_participation?).to eq('Audience, the speaker could not make it!')
-    #   end
-    # end
-    #
-    #
-    #
-    # # No knowledge people are shy
-    # context 'when participation count is outside of the TOLERANCE' do
-    #   let(:no_knowledge) { 5 }
-    #
-    #   it 'Is outside the TOLERANCE & returns FALSE' do
-    #     expect(checker.valid_participation?).to eq(false)
-    #   end
-    # end
-    #
-    # #knowledge people are modest
-    # context 'when participation count is outside of the TOLERANCE' do
-    #   let(:knowledge) { 5 }
-    #
-    #   it 'Is outside the TOLERANCE & returns FALSE' do
-    #     expect(checker.valid_participation?).to eq(false)
-    #   end
-    # end
-    #
-    #
-    # context 'when audience count is not Integer' do
-    #   let(:audience_count) { '20' }
-    #
-    #   it 'returns false' do
-    #     expect(checker.valid_participation?).to eq("Don't give me rubbish")
-    #   end
-    # end
-    #
-    # context 'when knowledge count is not Integer' do
-    #   let(:knowledge) { '10' }
-    #
-    #   it 'returns false' do
-    #     expect(checker.valid_participation?).to eq("Don't give me rubbish")
-    #   end
-    # end
-    #
-    # context 'when NO knowledge count is not Integer' do
-    #   let(:no_knowledge) { '10' }
-    #
-    #   it 'returns false' do
-    #     expect(checker.valid_participation?).to eq("Don't give me rubbish")
-    #   end
-    # end
-    #
-    # context 'when SPEAKER_COUNT count is not Integer' do
-    #   let(:speaker_count) { '1' }
-    #
-    #   it 'returns false' do
-    #     expect(checker.valid_participation?).to eq("Don't give me rubbish")
-    #   end
-    # end
+
   end
 end
